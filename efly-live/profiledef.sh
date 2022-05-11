7e565a82c0dd235a80568a28ab36f82a02e6666c
flying-dude <flying-dude-11@protonmail.com>
2022-05-11 22:04:55 +0200

    remove .automated_script.sh

--- efly-live/profiledef.sh
+++ efly-live/profiledef.sh
@@ -19,7 +19,6 @@ file_permissions=(
   ["/etc/gshadow"]="0:0:0400"
 
   ["/root"]="0:0:750"
-  ["/root/.automated_script.sh"]="0:0:755"
 
   ["/usr/local/bin/choose-mirror"]="0:0:755"
   ["/usr/local/bin/Installation_guide"]="0:0:755"
